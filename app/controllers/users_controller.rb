d1ccd8e0dcb228cbffc17e1b9c80d351e701a54b
Ryan Rotz <ryanrotz@gmail.com>
2016-07-13 18:48:45 -0700

    signup form works on /users/new and returns a json object

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
   def index
-    @user = User.new
+    
   end
 
   def new
+    @user = User.new
   end
 
   def show
   end
 
   def create
+    render json: params
   end
 
   def edit
