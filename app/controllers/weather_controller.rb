1e0ede48a780f38635f4fb20c91e28fdce2c3ae0
Thomas <evans_760@yahoo.com>
2016-07-14 16:24:46 -0700

    Added api call

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,14 @@
 class WeatherController < ApplicationController
+  
   def index
+
+data = RestClient.get("http://magicseaweed.com/api/deecd9b63d7991728f562f51a86f7b79/forecast/?spot_id=308&units=us", {
+
+})
+
+@data = JSON.parse(data)
+
+# puts '**********', @data
+# render :json => @data
   end
 end
