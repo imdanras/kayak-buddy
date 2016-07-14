7b09ec8ce9f606a8e493290938ffe422c92104a7
Ryan Rotz <ryanrotz@gmail.com>
2016-07-14 16:44:20 -0700

    auth is working. Added login form. Flash messages working but unstyled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     # render json: params
     user = User.new(user_params)
     if user.save
-      # session[:user_id] = user.id
+      session[:user_id] = user.id
       redirect_to root_path
     else
       flash[:Error] = user.errors.messages
