1818c986b9a432f47653e642f49de28de33f828a
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-07-14 10:14:49 -0700

    added weather view and controller"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'weather/index'
+
   root "users#index"
 
   resources :users
@@ -8,7 +10,9 @@ Rails.application.routes.draw do
 
   get 'sessions/new', to: 'sessions#index'
   post 'sessions/create', to: 'sessions#create'
-  delete 'sessions/destroy', to: 'sessions#destroy' 
+  delete 'sessions/destroy', to: 'sessions#destroy'
+
+  get 'weather', to: 'weather#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
