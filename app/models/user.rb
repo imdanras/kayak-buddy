4671955050263c203a761caf5b36b606ed643b31
Thomas <evans_760@yahoo.com>
2016-07-14 10:22:29 -0700

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ApplicationRecord
+  has_many :groups
   has_secure_password
+
+  validates :email,
+  presence: true,
+  uniqueness: {case_sensitive: false}
+
+  validates :password,
+  length: { in: 8..72 },
+  on: :create
+
+  validates :name,
+  presence: true,
+  length: { in: 2...20 }
+
+
+  def self.authenticate(params)
+    User.find_by_email(params[:email]).try(:authenticate, params[:password])
+end
+
 end
