6cf62237e7c469af93c5cc215c3acd764300a723
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-07-15 12:06:28 -0700

    added emailer functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  #set up email contact sender
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
